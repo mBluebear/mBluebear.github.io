a32cdfa318d9f65668f796b70649a426019e030f
mBluebear <tquynh1112@yahoo.com>
2014-11-18 08:19:12 -0500

    Added the rest of the contents
    Added the rest of the page contents

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    $('#slideshow-container').slick({
+        dots: true
+    });
+
+    var wall = new freewall("#portfolio_grid");
+    wall.reset({
+        selector: '.item',
+        animate: true,
+        cellW: 200,
+        cellH: 'auto',
+        onResize: function() {
+            wall.fitWidth();
+        }
+    });
+
+    var images = wall.container.find('.item');
+    images.find('img').load(function() {
+        wall.fitWidth();
+    });
+
+    $('#portfolio_grid .item').click(function(){
+        $('#mask').fadeIn();
+        $('#port_' + $(this).attr('data-id')).fadeIn();
+        $('#port_' + $(this).attr('data-id')).css({top: $(this).offset().top + 'px'});
+
+
+        $('.portfolio_slide').slick({
+            dots: true,
+            prevArrow: '',
+            nextArrow: '',
+            //lazyLoad: 'ondemand'
+        });
+    });
+
+    $('#mask').click(function(){
+        $('#mask').fadeOut();
+        $('.portfolio_overlay').fadeOut();
+    });
+
+    $('#phone-nav').click(function(){
+        $('#mobile-nav').slideToggle();
+        return false;
+    });
+
+    $('.nav a, #mobile-nav a').click(function(){
+        var xxx = $(this);
+        $('html, body').animate({
+            scrollTop: $(xxx.attr('href')).offset().top - 50
+        }, 500);
+
+        $('.nav li').removeClass('active');
+        xxx.parent().addClass('active');
+        $('#mobile-nav').slideUp();
+
+        return false;
+    });
+});
